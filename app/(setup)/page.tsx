a0578fe59c5e90f757859be7fd1c9d38ed3052c6
thegoodgamer14 <aryan.sgrbk@gmail.com>
2024-07-13 20:13:24 +0400

    accepting file-uploads. server image upload added

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation"
 import { db } from "@/lib/db"
 import { intialProfile } from "@/lib/intial-profile"
+import { InitialModal } from "@/components/modals/initial-modal"
 
 const SetupPage = async () => {
     const profile = await intialProfile()
@@ -19,7 +20,7 @@ const SetupPage = async () => {
         return redirect(`/servers/${server.id}`)
     }
 
-    return <div>Create a Server</div>
+    return <InitialModal/>
 }
 
 export default SetupPage

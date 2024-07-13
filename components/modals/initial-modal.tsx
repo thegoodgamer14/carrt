326f0070e7df3b4489f7195236b7fd6b79dffce7
thegoodgamer14 <aryan.sgrbk@gmail.com>
2024-07-13 20:36:28 +0400

    server creation API added

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import axios from "axios"
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -10,6 +11,7 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { FileUpload } from "@/components/file-upload"
+import { useRouter } from "next/navigation"
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -22,6 +24,8 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
     const [isMounted, setIsMounted] = useState(false)
+    const router = useRouter()
+
     useEffect(() => {
         setIsMounted(true)
     }, [])
@@ -36,7 +40,14 @@ export const InitialModal = () => {
 
     const isLoading = form.formState.isSubmitting
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+        try {
+            await axios.post("/api/servers", values)
+            form.reset()
+            router.refresh()
+            window.location.reload()
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     if (!isMounted){

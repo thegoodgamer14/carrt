2f492ebb8c3ed2bf54c687ddbd8b861530333f8e
thegoodgamer14 <aryan.sgrbk@gmail.com>
2024-07-16 23:20:22 +0400

    added server modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
-import { ThemeProvider } from '@/components/providers/theme-provider'
 import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ModalProvider } from '@/components/providers/modal-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -21,7 +22,13 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
-          <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false} storageKey='carrt-theme'>
+          <ThemeProvider 
+            attribute='class'
+            defaultTheme='dark'
+            enableSystem={false}
+            storageKey='carrt-theme'
+          >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>

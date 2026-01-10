e359573294617d1d9966699bda4896e649ff05b9
thegoodgamer14 <aryan.sgrbk@gmail.com>
2026-01-10 20:35:48 +0400

    added invite server modal
    - link generation and copying
    - updated modal provider and store
    - added new use origin hook to get server id param

---
+++ components/modals/invite-modal.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import { useModal } from "@/hooks/use-modal-store"
+import { Check, Copy, RefreshCcw } from "lucide-react"
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { useOrigin } from "@/hooks/use-origin"
+import axios from "axios"
+import { useState } from "react"
+
+export const InviteModal = () => {
+    const { onOpenisOpen, onClose, type, data } = useModal()
+    const origin = useOrigin()
+    const isModalOpen = isOpen && type === "invite"
+    const { server } = data
+    const [copied, setCopied] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+    const inviteUrl = `${origin}/invite/${server?.inviteCode}`
+
+    const onCopy = () => {
+        navigator.clipboard.writeText(inviteUrl)
+        setCopied(true)
+        setTimeout(() => {
+            setCopied(false)
+        }, 1000)
+    }
+
+    const onNew = async () => {
+        try {
+            setIsLoading(true)
+            const response = await axios.patch(`/api/servers/${server?.id}/invite`)
+
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    return (
+        <Dialog open={isModalOpen} onOpenChange={onClose}>
+            <DialogContent className="bg-white text-black p-0 overflow-hidden">
+                <DialogHeader className="pt-8 px-6">
+                    <DialogTitle className="text-2xl text-center font-bold">
+                        Invite Friends
+                    </DialogTitle>
+                </DialogHeader>
+                <div className="p-6">
+                    <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                        Server Invite Link
+                    </Label>
+                    <div className="flex items-center mt-2 gap-x-2">
+                        <Input className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0" value={inviteUrl} />
+                        <Button onClick={onCopy} size="icon">
+                            {copied ? <Check className="w-4 h-4" /> : <Copy className="w-4 h-4" />}
+                        </Button>
+                    </div>
+                    <Button variant="link" size="sm" className="text-xs text-zinc-500 mt-4">
+                        Generate a new link
+                        <RefreshCcw className="w-4 h-4 ml-2" />
+                    </Button>
+                </div>
+            </DialogContent>
+        </Dialog>
+    )
+}

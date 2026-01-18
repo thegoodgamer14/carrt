dc6c1fb17593f0a772c3b46ee81c61d172bbb3b6
Aryan Sharma <85781882+thegoodgamer14@users.noreply.github.com>
2026-01-19 01:31:04 +0400

    added leave and delete server modals

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -60,13 +60,13 @@ export const ServerHeader = ({
                     <DropdownMenuSeparator />
                 )}
                 {isAdmin && (
-                    <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem onClick={() => onOpen("deleteServer", {server})} className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Delete Server
                         <Trash className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
                 )}
                 {!isAdmin && (
-                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer text-rose-500">
+                    <DropdownMenuItem onClick={() => onOpen("leaveServer", {server})} className="px-3 py-2 text-sm cursor-pointer text-rose-500">
                         Leave Server
                         <LogOut className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>

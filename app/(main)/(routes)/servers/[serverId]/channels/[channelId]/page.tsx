de05e9054e7688fcc7852fa34ba8872d221d4530
Aryan Sharma <85781882+thegoodgamer14@users.noreply.github.com>
2026-01-30 15:21:14 +0400

    added messages api and chat input

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import { ChatHeader } from "@/components/chat/chat-header"
+import { ChatInput } from "@/components/chat/chat-input"
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db"
 import { redirectToSignIn } from "@clerk/nextjs"
@@ -35,6 +36,18 @@ const ChannelIdPage = async ({
     return (
         <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
             <ChatHeader serverId={channel.serverId} name={channel.name} type="channel" />
+            <div className="flex-1">
+                Future Messages
+            </div>
+            <ChatInput
+                type="channel"
+                apiUrl="/api/socket/messages"
+                query={{
+                    channelId: channel.id,
+                    serverId: channel.serverId
+                }}
+                name={channel.name}
+            />
         </div>
     )
 }

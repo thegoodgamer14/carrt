c733f12dd0779211ece7421060a4603c04330c91
Aryan Sharma <85781882+thegoodgamer14@users.noreply.github.com>
2026-01-30 14:12:31 +0400

    socket.io setup for live chats

--- types.ts
+++ types.ts
@@ -1,7 +1,18 @@
+import { Server as NetServer, Socket } from "net"
+import { NextApiResponse } from "next"
+import { Server as SocketIOServer } from "socket.io"
 import { Profile, Server, Member } from "@prisma/client"
 
 export type ServerWithMembersWithProfiles = Server & {
     members: (Member & {
         profile: Profile
     })[]
+}
+
+export type NextApiResponseServerIO = NextApiResponse & {
+    socket: Socket & {
+        server: NetServer & {
+            io: SocketIOServer
+        }
+    }
 }

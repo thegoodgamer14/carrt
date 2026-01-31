add38152a8c676689ad36c509bf505fba3115d4e
Aryan Sharma <85781882+thegoodgamer14@users.noreply.github.com>
2026-01-31 17:21:46 +0400

    fixed build error in create channel modal

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -76,7 +76,7 @@ export const CreateChannelModal = () => {
         onClose()
     }
 
-    return(
+    return (
         <Dialog open={isModalOpen} onOpenChange={handleClose}>
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
                 <DialogHeader className="pt-8 px-6">
@@ -87,7 +87,7 @@ export const CreateChannelModal = () => {
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
-                            <FormField control={form.control} name="name" render={({ field }) =>(
+                            <FormField control={form.control} name="name" render={({ field }) => (
                                 <FormItem>
                                     <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
                                         Channel name
@@ -98,29 +98,27 @@ export const CreateChannelModal = () => {
                                     <FormMessage />
                                 </FormItem>
                             )
-                        }/>
-                        <FormField control={form.control} name="type" render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Channel Type</FormLabel>
-                                <Select disabled={isLoading} onValueChange={field.onChange} defaultValue={field.value}>
-                                    <FormControl>
-                                        <SelectTrigger className="bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0 focus:ring-offset-0 capitalize outline-none">
-                                            <SelectValue placeholder="Select a channel type" />
-                                        </SelectTrigger>
-                                    </FormControl>
-                                    <SelectContent>
-                                        {Object.values(ChannelType).map((type) => (
-                                            <SelectItem key={type} value={type} className="capitalize">
-                                                {type.toLowerCase()}
-                                            </SelectItem>
-                                        ))}
-                                    </SelectContent>
-                                </Select>
-                                <FormMessage />
-                            </FormItem>
-                        )}>
-
-                        </FormField>
+                            } />
+                            <FormField control={form.control} name="type" render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Channel Type</FormLabel>
+                                    <Select disabled={isLoading} onValueChange={field.onChange} defaultValue={field.value}>
+                                        <FormControl>
+                                            <SelectTrigger className="bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0 focus:ring-offset-0 capitalize outline-none">
+                                                <SelectValue placeholder="Select a channel type" />
+                                            </SelectTrigger>
+                                        </FormControl>
+                                        <SelectContent>
+                                            {Object.values(ChannelType).map((type) => (
+                                                <SelectItem key={type} value={type} className="capitalize">
+                                                    {type.toLowerCase()}
+                                                </SelectItem>
+                                            ))}
+                                        </SelectContent>
+                                    </Select>
+                                    <FormMessage />
+                                </FormItem>
+                            )} />
                         </div>
                         <DialogFooter className="bg-gray-100 px-6 py-4" >
                             <Button variant="primary" disabled={isLoading}>
